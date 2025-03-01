8866b2fee59fdde5f049ccbbb1fe0ca282d44de8
unknown <srimandgl2004@gmail.com>
2025-03-01 11:37:49 +0530

    completed Adding-relations-between-entites

---
+++ src/components/ui/card.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './card.css';
+
+const Card = ({ children, className }) => {
+  return <div className={`card ${className}`}>{children}</div>;
+};
+
+const CardHeader = ({ children }) => {
+  return <div className="card-header">{children}</div>;
+};
+
+const CardTitle = ({ children }) => {
+  return <h2 className="card-title">{children}</h2>;
+};
+
+const CardDescription = ({ children }) => {
+  return <p className="card-description">{children}</p>;
+};
+
+const CardContent = ({ children }) => {
+  return <div className="card-content">{children}</div>;
+};
+
+export { Card, CardHeader, CardTitle, CardDescription, CardContent };
